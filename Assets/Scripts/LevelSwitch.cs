b0fc56047afe3319b5657b3fd903b1264aeae8b9
SniperPig <alexandrurotaru16@gmail.com>
2021-10-22 09:42:28 -0400

    Added Level Transition and light in mine

---
+++ Assets/Scripts/LevelSwitch.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSwitch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int scene = 0;
+    void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene(scene);
+    }
+        
+
+}
