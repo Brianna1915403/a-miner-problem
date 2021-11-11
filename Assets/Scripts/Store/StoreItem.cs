a66ca3efb4b34f8ea45574d36697a219d789215b
Salam <56936701+Salam07@users.noreply.github.com>
2021-11-10 22:13:13 -0500

    Script for storeManager first step

--- Assets/Scripts/Store/StoreItem.cs
+++ Assets/Scripts/Store/StoreItem.cs
@@ -2,17 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StoreItem : MonoBehaviour
+public class StoreItem
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public enum ItemType{
+        Pickaxe,
+        TrainPart,
+        Wagon
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public static int[] GetCost(ItemType itemType){
+        switch(itemType){
+            default:
+            case ItemType.Pickaxe:   return new int[]{0,0,0,0,0};
+            case ItemType.TrainPart: return new int[]{0,0,0,0,0};
+            case ItemType.Wagon:     return new int[]{0,0,0,0,0};
+        }
     }
+
+    // public static Sprite GetSprite(ItemType itemType){
+    //     switch (itemType)
+    //     {
+    //         default:
+    //         case ItemType.Pickaxe:   return GameAssets.i.s_Pickaxe;
+    //         case ItemType.TrainPart: return GameAssets.i.s_TrainPart;
+    //         case ItemType.Wagon:     return GameAssets.i.s_Wagon;
+    //     }
+    // }
 }
--- Assets/Scripts/Store/StoreManagement.cs
+++ Assets/Scripts/Store/StoreManagement.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class StoreManagement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    private Transform container;
+    private Transform shopItemTemplate;
+    
+    private void Awake(){
+        container = transform.Find("container");
+        shopItemTemplate = container.Find("shopItemTemplate");
+        shopItemTemplate.gameObject.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    private void CreateItemButton(Sprite itemSprite, string itemName, int[] itemCost, int positionIndex){
+        Transform shopItemTransform = Instantiate(shopItemTemplate, container);
+        RectTransform shopItemRectTransform = shopItemTransform.GetComponent<RectTransform>();
+
+        float shopItemHeight = 30f;
+        shopItemRectTransform.anchoredPosition = new Vector2(0, -shopItemHeight * positionIndex);
+
+        shopItemTransform.Find("itemName").GetComponent<TextMeshProUGUI>().SetText(itemName);
         
+        shopItemTransform.Find("copperPriceText").GetComponent<TextMeshProUGUI>().SetText(itemCost[0].ToString());
+        shopItemTransform.Find("silverPriceText").GetComponent<TextMeshProUGUI>().SetText(itemCost[1].ToString());
+        shopItemTransform.Find("goldPriceText").GetComponent<TextMeshProUGUI>().SetText(itemCost[2].ToString());
+        shopItemTransform.Find("electrumPriceText").GetComponent<TextMeshProUGUI>().SetText(itemCost[3].ToString());
+        shopItemTransform.Find("platinumPriceText").GetComponent<TextMeshProUGUI>().SetText(itemCost[4].ToString());
+
+        shopItemTransform.Find("itemImage").GetComponent<Image>().sprite = itemSprite;
     }
 }
