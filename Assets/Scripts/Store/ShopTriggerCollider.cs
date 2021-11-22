1bb32d74fdd95f41486551946470ffb4d0202f3b
Salam <salambensouki@hotmail.com>
2021-11-22 17:43:30 -0500

    spawn the store when approaching the store in the test world

--- Assets/Scripts/Store/ShopTriggerCollider.cs
+++ Assets/Scripts/Store/ShopTriggerCollider.cs
@@ -6,16 +6,34 @@ public class ShopTriggerCollider : MonoBehaviour
 {
     public GameObject ShopPanel;
 
-    private void OnControllerColliderHit(ControllerColliderHit hit) {
-        if(hit.transform.tag == "Shop"){
-            if(Input.GetKeyDown("q")){
-                Debug.Log("The shop is open");
-                ShopPanel.SetActive(true);
-            }
-            if(Input.GetKeyDown("z")){
-                Debug.Log("The shop is close");
-                ShopPanel.SetActive(false);
+    public float m_Radius = 5f;
+
+    private void Update() {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, m_Radius);
+        foreach(Collider collider in colliders){
+            if(collider.CompareTag("Player")){
+                Debug.Log("Orphans in my basement");
+                if(Input.GetKeyDown("q")){
+                    Debug.Log("The shop is open");
+                    ShopPanel.SetActive(true);
+                    Cursor.visible = true;
+                    Cursor.lockState = CursorLockMode.None;
+                    Time.timeScale = 0;
+                }
+                if(Input.GetKeyDown("z")){
+                    Debug.Log("The shop is close");
+                    ShopPanel.SetActive(false);
+                    Cursor.visible = false;
+                    Cursor.lockState = CursorLockMode.Locked;
+                    Time.timeScale = 1  ;
+                }
             }
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, m_Radius);
     }
 }
